09aa0e0702f3251ca8191388dbb8b985626200b7
Amit Dhiman <amitdhiman5086@gmail.com>
2024-09-24 22:14:43 +0530

    Create Get User Api Route

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -79,3 +79,44 @@ export const signOut = (req, res, next) => {
     next(error);
   }
 };
+
+export const getUsers = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, "You are not allowed to see all users"));
+  }
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 10;
+    const sortDirection = req.query.sort === "asc" ? 1 : -1;
+
+    const users = await User.find()
+      .sort({ createdAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const userWithoutPassword = users.map((user) => {
+      const { password, ...rest } = user._doc;
+      return rest;
+    });
+
+    const totalUser = await User.countDocuments();
+
+    const now = new Date();
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+    const lastMonthUser = await User.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      user: userWithoutPassword,
+      totalUser,
+      lastMonthUser,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
