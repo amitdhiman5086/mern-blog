11c19d1ee82cc93c5dd7b17dba4c4ffc5561769b
Amit Dhiman <amitdhiman5086@gmail.com>
2024-10-26 19:06:27 +0530

    Add pre Deploy Things

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -104,7 +104,7 @@ export default function Header() {
             <Dropdown.Item onClick={handleSignout}>Sign out</Dropdown.Item>
           </Dropdown>
         ) : (
-          <Link to="/sign-in">
+          <Link to="/signin">
             <Button gradientDuoTone="purpleToBlue" outline>
               Sign In
             </Button>
