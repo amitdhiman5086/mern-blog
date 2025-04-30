c1c8b5e25440ae96aca22973ef00404adee882b6
amitdhiman5086 <amitdhiman5086@gmail.com>
2025-04-30 19:37:43 +0530

    Every 10  minutes

--- backend/utils/cron.js
+++ backend/utils/cron.js
@@ -3,7 +3,7 @@ import https from "https";
 
 const URL = "https://zoo-blog.onrender.com/";
 
-const job = new cron.CronJob("*/1 * * * *", function () {
+const job = new cron.CronJob("*/10 * * * *", function () {
 	https
 		.get(URL, (res) => {
 			if (res.statusCode === 200) {
